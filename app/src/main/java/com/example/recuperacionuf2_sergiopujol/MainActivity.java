8b9cdedbb9f5f6d880ff3c5226877fedf8c3add7
Black Shadow <sergiopujol.98@gmail.com>
2021-06-06 20:42:30 +0200

    Camara

--- app/src/main/java/com/example/recuperacionuf2_sergiopujol/MainActivity.java
+++ app/src/main/java/com/example/recuperacionuf2_sergiopujol/MainActivity.java
@@ -1,14 +1,75 @@
 package com.example.recuperacionuf2_sergiopujol;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.core.content.FileProvider;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnCamara;
+    ImageView imgView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnCamara = findViewById(R.id.btnCamara);
+        imgView = findViewById(R.id.imageView);
+
+        if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.CAMERA}, 1000);
+        }
+
+        btnCamara.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                hacerFoto();
+            }
+        });
+    }
+
+    static final int REQUEST_TAKE_PHOTO = 1;
+    private void hacerFoto() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            File photoFile = null;
+            try {
+                photoFile = crearArchivoImagen();
+            } catch (IOException ex) {
+
+            }
+            if (photoFile != null) {
+                Uri photoURI = FileProvider.getUriForFile(this, "com.example.recuperacionuf2_sergiopujol", photoFile);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO);
+            }
+        }
+    }
+
+    String currentPhotoPath;
+    public File crearArchivoImagen() throws IOException {
+        String tiempo = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageName = "Foto_"+tiempo+"_";
+        File storage = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(imageName,".jpg",storage);
+        currentPhotoPath = image.getAbsolutePath();
+        return image;
     }
 }
